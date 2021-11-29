9629818dcd14b4d2703b6f9aca1d5fc6da4ccec5
Gilbert Brault <gbrault@seadev.org>
2021-11-29 10:30:28 +0100

    Update base_widget.py

--- ipybus/base_widget.py
+++ ipybus/base_widget.py
@@ -20,9 +20,9 @@ class Base(widgets.DOMWidget):
     _model_module = Unicode('ipybus').tag(sync=True)
 
     # Version of the front-end module containing widget view
-    _view_module_version = Unicode('^0.1.0').tag(sync=True)
+    _view_module_version = Unicode('^0.1.2').tag(sync=True)
     # Version of the front-end module containing widget model
-    _model_module_version = Unicode('^0.1.0').tag(sync=True)
+    _model_module_version = Unicode('^0.1.2').tag(sync=True)
 
     # Widget specific property.
     # Widget properties are defined as traitlets. Any property tagged with `sync=True`
